f2e6e9fa6949786e7a183f48aa0c07d8a3d6a2d1
Martin MÃller <mav.northwind@web.de>
2021-04-03 07:54:25 +0200

    Alle http requests auf Axios umgestellt

--- node_helper.js
+++ node_helper.js
@@ -47,7 +47,6 @@ module.exports = NodeHelper.create({
 	
 	socketNotificationReceived: function(notification, payload)	{
 		var self = this;
-		// console.log(`node_helper ${self.name}: socketNotification ${notification} received`);
 
 		switch (notification) {
 			case "CONFIG":
@@ -112,125 +111,103 @@ module.exports = NodeHelper.create({
 
 	fetchPowerFlow: function() {
 		var self = this;
-		//console.log(`node_helper ${self.name}: trying to get powerFlow`);
 
-		try{
-			if (!self.config)
-				console.error(`node_helper ${self.name}:Configuration has not been set!`);
-
-			var monitoringUrl = `https://monitoringapi.solaredge.com/site/${self.config.siteId}/currentPowerFlow?format=application/json&api_key=${self.config.apiKey}`;
-			//console.log(`node_helper ${self.name}: get powerFlow from ${monitoringUrl}`);
-
-			request({
-				url: monitoringUrl,
-				method: 'GET'
-			}, function (error, response, body) {
-				if (error) {
-					console.error(`node_helper ${self.name}: Could not get PowerFlow: ${error}`);
-					self.sendSocketNotification("PVERROR", error);
-					return;
-				}
-				if (response.statusCode >= 400 && response.statusCode < 500) {
-					console.error(`node_helper ${self.name}: request returned status ${response.statusCode}`);
-					self.sendSocketNotification("PVERROR", body);
-					return;
-				}
-				if (response.statusCode == 200) {
-					console.log(`node_helper ${self.name}: got powerFlow data: ${JSON.stringify(response)}`);
-
-					var reply = JSON.parse(body);
-					var powerflow = reply.siteCurrentPowerFlow;
-
-					if (self.maxPower.value < powerflow.PV.currentPower) {
-						self.maxPower.value = powerflow.PV.currentPower;
-						self.maxPower.timestamp = Date.now();
-						fs.writeFileSync("maxPower.json", JSON.stringify(self.maxPower));
-					}
-
-					var d = new Date();
-					if (self.productionSpan.day != d.getDay()) {
-						self.productionSpan.day = d.getDay();
-						self.productionSpan.firstProduction = "-";
-						self.productionSpan.lastProduction = "-";
-					}
-					if (self.productionSpan.firstProduction == "-" &&
-					    powerflow.PV.currentPower > 0) {
-						self.productionSpan.firstProduction = `${d.toLocaleTimeString()}`;
-					}
-					if (self.productionSpan.firstProduction != "-" &&
-					    self.productionSpan.lastProduction == "-" &&
-						powerflow.PV.currentPower <= 0 &&
-						d.getHours() > 10) { // avoid initial jitter
-						self.productionSpan.lastProduction = `${d.toLocaleTimeString()}`;
-					}
-
-					var powerflowReply = {
-						powerflow: powerflow,
-						productionSpan: self.productionSpan,
-						requestCount: self.throttler.todaysCallCount
-					};
-
-					self.sendSocketNotification("POWERFLOW", powerflowReply);
-					// console.log(`node_helper ${self.name}: sent powerflow ${JSON.stringify(powerflowReply)}`);
-				}
-			});
-		} catch(ex)
-		{
-			console.error(`node_helper ${self.name}: error ${ex}`);
-			self.sendSocketNotification("PVERROR", ex);
+		if (!self.config){
+			console.error(`node_helper ${self.name}:Configuration has not been set!`);
+			return;
 		}
+
+		var powerFlowUrl = `https://monitoringapi.solaredge.com/site/${self.config.siteId}/currentPowerFlow`;
+		axios.get(powerFlowUrl, {
+			params: {
+				format: "application/json",
+				api_key: self.config.apiKey,
+			}})
+		.then(res => {
+			console.log(`node_helper ${self.name}: got powerFlow data: ${JSON.stringify(res.data)}`);
+
+			var reply = res.data;
+			var powerflow = reply.siteCurrentPowerFlow;
+
+			if (self.maxPower.value < powerflow.PV.currentPower) {
+				self.maxPower.value = powerflow.PV.currentPower;
+				self.maxPower.timestamp = Date.now();
+				fs.writeFileSync("maxPower.json", JSON.stringify(self.maxPower));
+			}
+
+			var d = new Date();
+			if (self.productionSpan.day != d.getDay()) {
+				self.productionSpan.day = d.getDay();
+				self.productionSpan.firstProduction = "-";
+				self.productionSpan.lastProduction = "-";
+			}
+			if (self.productionSpan.firstProduction == "-" &&
+				powerflow.PV.currentPower > 0) {
+				self.productionSpan.firstProduction = `${d.toLocaleTimeString()}`;
+			}
+			if (self.productionSpan.firstProduction != "-" &&
+				self.productionSpan.lastProduction == "-" &&
+				powerflow.PV.currentPower <= 0 &&
+				d.getHours() > 10) { // avoid initial jitter
+				self.productionSpan.lastProduction = `${d.toLocaleTimeString()}`;
+			}
+
+			var powerflowReply = {
+				powerflow: powerflow,
+				productionSpan: self.productionSpan,
+				requestCount: self.throttler.todaysCallCount
+			};
+
+			self.sendSocketNotification("POWERFLOW", powerflowReply);
+		})
+		.catch(err => {
+			console.error(`node_helper ${self.name}: request returned error  ${err}`);
+			self.sendSocketNotification("PVERROR", err);
+		});
 	},
 
 	fetchProduction: function() {
 		var self = this;
 
-		try{
-			if (!self.config)
-				console.error(`node_helper ${self.name}:Configuration has not been set!`);
-
-			var today = new Date();
-			var yesterday = new Date(Date.now() - 24*60*60000);
-			var startDate = yesterday.toJSON().substr(0,10);
-			var endDate = today.toJSON().substr(0,10);
-			var monitoringUrl = `https://monitoringapi.solaredge.com/site/${self.config.siteId}/energy?format=application/json&api_key=${self.config.apiKey}&timeUnit=DAY&startDate=${startDate}&endDate=${endDate}`;
-
-			request({
-				url: monitoringUrl,
-				method: 'GET'
-			}, function (error, response, body) {
-				if (error) {
-					console.error(`node_helper ${self.name}: Could not get Production: ${error}`);
-					self.sendSocketNotification("PVERROR", error);
-					return;
-				}
-				if (response.statusCode >= 400 && response.statusCode < 500) {
-					console.error(`node_helper ${self.name}: request returned status ${response.statusCode}`);
-					self.sendSocketNotification("PVERROR", body);
-					return;
-				}
-				if (response.statusCode == 200) {
-					console.log(`node_helper ${self.name}: got energy data: ${JSON.stringify(response)}`);
-
-					var reply = JSON.parse(body);
-					var energy = reply.energy;
-					var prodYesterday = self.findProductionForDay(startDate, energy.values);
-					var prodToday = self.findProductionForDay(endDate, energy.values);
-
-					var productionReply = {
-						unit: energy.unit,
-						productionToday: prodToday,
-						productionYesterday: prodYesterday
-					};
-
-					self.sendSocketNotification("PRODUCTION", productionReply);
-					// console.log(`node_helper ${self.name}: sent production ${JSON.stringify(productionReply)}`);
-				}
-			});
-		} catch(ex)
-		{
-			console.error(`node_helper ${self.name}: error ${ex}`);
-			self.sendSocketNotification("PVERROR", ex);
+		if (!self.config){
+			console.error(`node_helper ${self.name}:Configuration has not been set!`);
+			return;
 		}
+
+		var today = new Date();
+		var yesterday = new Date(Date.now() - 24*60*60000);
+		var startDate = yesterday.toJSON().substr(0,10);
+		var endDate = today.toJSON().substr(0,10);
+		var energyUrl = `https://monitoringapi.solaredge.com/site/${self.config.siteId}/energy`;
+
+		axios.get(energyUrl, {
+			params: {
+				format: "application/json",
+				api_key: self.config.apiKey,
+				timeUnit: "DAY",
+				startDate: startDate,
+				endDate: endDate
+			}})
+		.then(res => {
+			console.log(`node_helper ${self.name}: got energy data: ${JSON.stringify(res.data)}`);
+
+			var reply = res.data;
+			var energy = reply.energy;
+			var prodYesterday = self.findProductionForDay(startDate, energy.values);
+			var prodToday = self.findProductionForDay(endDate, energy.values);
+
+			var productionReply = {
+				unit: energy.unit,
+				productionToday: prodToday,
+				productionYesterday: prodYesterday
+			};
+
+			self.sendSocketNotification("PRODUCTION", productionReply);
+		})
+		.catch(err => {
+			console.error(`node_helper ${self.name}: request returned error  ${err}`);
+			self.sendSocketNotification("PVERROR", err);
+		});
 	},
 
 	fetchSiteDetails: function() {
