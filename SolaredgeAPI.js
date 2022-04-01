9c8ece7adef86a86d50d83f394ad3060b71a8042
Martin MÃller <mav.northwind@web.de>
2022-04-01 20:33:25 +0200

    Begonnen, die node_helper.js auf SolaredgeAPI umzustellen

--- SolaredgeAPI.js
+++ SolaredgeAPI.js
@@ -48,7 +48,13 @@ class SolaredgeAPI {
         fs.writeFileSync("maxPower.json", JSON.stringify(this.#maxPower));
     }
 
-    
+    //
+    // siteDetails {
+    // 	name: "SITENAME"
+	// 	peakPower: 0.0,
+	//  maxPower: { value: 0.0, timestamp: 1648818482783 }
+    // }
+    //
     async fetchSiteDetails() {
         var siteDetailsUrl = `https://monitoringapi.solaredge.com/site/${this.#siteId}/details`;
 
@@ -60,8 +66,7 @@ class SolaredgeAPI {
 		.then(res => {
 			console.debug(`Got SiteDetail data: ${JSON.stringify(res.data)}`);
 
-			var reply = res.data;
-			var details = reply.details;
+			var details = res.data.details;
 
             return {
 				name: details.name,
@@ -70,7 +75,61 @@ class SolaredgeAPI {
 			};
 		})
 		.catch(err => {
-			console.error(`Request returned error  ${err}`);
+			console.error(`Request for powerflow on ${this.#siteId} returned error  ${err}`);
+		});
+
+        return ret;
+    }
+
+    //
+    //
+    // powerFlow {
+    //  
+    // }
+    //
+    //
+    //
+    async fetchCurrentPowerFlow() {
+        var powerFlowUrl = `https://monitoringapi.solaredge.com/site/${this.#siteId}/currentPowerFlow`;
+
+		var ret = await axios.get(powerFlowUrl, {
+			params: {
+				format: "application/json",
+				api_key: this.#apiKey,
+			}})
+		.then(res => {
+			console.debug(`Got powerFlow data: ${JSON.stringify(res.data)}`);
+
+			var currentPowerflow = res.data.siteCurrentPowerFlow;
+
+			if (this.maxPower.value < currentPowerflow.PV.currentPower) {
+				this.maxPower = currentPowerflow.PV.currentPower;
+			}
+
+            // productionSpan erst mal rauslassen
+            //
+			// var d = new Date();
+			// if (self.productionSpan.day != d.getDay()) {
+			// 	self.productionSpan.day = d.getDay();
+			// 	self.productionSpan.firstProduction = "-";
+			// 	self.productionSpan.lastProduction = "-";
+			// }
+			// if (self.productionSpan.firstProduction == "-" &&
+			// 	powerflow.PV.currentPower > 0) {
+			// 	self.productionSpan.firstProduction = `${d.toLocaleTimeString()}`;
+			// }
+			// if (self.productionSpan.firstProduction != "-" &&
+			// 	self.productionSpan.lastProduction == "-" &&
+			// 	powerflow.PV.currentPower <= 0 &&
+			// 	d.getHours() > 10) { // avoid initial jitter
+			// 	self.productionSpan.lastProduction = `${d.toLocaleTimeString()}`;
+			// }
+
+			 return currentPowerflow;
+		})
+		.catch(err => {
+			console.error(`Request for powerflow returned error  ${err}`);
+            return undefined;
 		});
 
         return ret;
